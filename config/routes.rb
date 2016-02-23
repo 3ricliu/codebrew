f297cd4d6bf48009fc7e38ac4ffc933b4980553f
3ricliu <ericliu4943@gmail.com>
2016-02-23 12:11:29 -0800

    creates user auth and session management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#new'
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
