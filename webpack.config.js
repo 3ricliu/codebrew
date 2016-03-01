0eef731f6860d0c19772efee4c7a414b17485067
3ricliu <ericliu4943@gmail.com>
2016-03-01 09:57:38 -0800

    Remove public assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/codeBrew.jsx",
+  entry: [
+    "./frontend/codeBrew"
+  ],
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
