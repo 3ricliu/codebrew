8f77382e5711497957c46e8e1be740fdbfe053f1
3ricliu <ericliu4943@gmail.com>
2016-03-01 16:32:14 -0800

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/codeBrew",
+  entry: "./frontend/codeBrew.jsx",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
