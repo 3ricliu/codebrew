c8d86ce00acffb066a45c1b4f3f8a1fe3e333745
3ricliu <ericliu4943@gmail.com>
2016-03-01 16:56:17 -0800

    Rename entry

---
+++ frontend/code_brew.jsx
@@ -0,0 +1,49 @@
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    ReactRouter = require('react-router'),
+    Router = ReactRouter.Router,
+    Route = ReactRouter.Route,
+    IndexRoute = ReactRouter.IndexRoute,
+    IndexRedirect = ReactRouter.IndexRedirect;
+
+var NoteStore = require('./stores/noteStore');
+
+var NotesIndex = require('./components/notes/notesIndex'),
+    NavBar = require('./components/navBar'),
+    NotebooksIndex = require('./components/notebooks/notebooksIndex'),
+    NotebookForm = require('./components/notebooks/notebookForm'),
+    NotebookIndexItem = require('./components/notebooks/notebookForm');
+
+var App = React.createClass({
+  render: function () {
+    return (<div className="app-container">
+        {this.props.children}
+        </div>
+    );
+  }
+});
+
+var routes = (
+  <Route path="/" component={App}>
+    <IndexRedirect to="home/notebooks" />
+    <Route path="home" component={NavBar}>
+      <Route path="notebooks" component={NotebooksIndex}>
+        <Route path="new" component={NotebookForm} />
+        <Route path="edit/:notebook_id" component={NotebookForm} />
+      </Route>
+      <Route path="notebooks/:notebook_id" component={NotebooksIndex}>
+        <Route path="notes" component={NotesIndex} />
+      </Route>
+    </Route>
+
+  </Route>
+);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var content = document.getElementById("content");
+  if (content) {
+    ReactDOM.render(<Router>{routes}</Router>,
+      content);
+  }
+});
